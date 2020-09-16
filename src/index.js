24bf4f8e0231c50b2e81c9dc44c108374b090ff6
Abdulhamid-Osman <47813658+Abdulhamid-Osman@users.noreply.github.com>
2020-09-16 10:14:08 +0530

    React internal and external css

--- src/index.js
+++ src/index.js
@@ -1,30 +1,30 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-const fname = "Abdulhamid";
-const lname = "Osman";
-const lucky = Math.floor(Math.random() * 100);
+const customeStyle = {
+  color: ""
+};
+
+let greetings;
+const time = new Date();
+const currentTime = time.getHours();
+
+if (currentTime < 12) {
+  greetings = "Goodmorning";
+  customeStyle.color = "red";
+} else if (currentTime < 18) {
+  greetings = "good Afternoon";
+  customeStyle.color = "green";
+} else {
+  greetings = "goodnight";
+  customeStyle.color = "blue";
+}
+
 ReactDom.render(
   <div>
-    <h1 className="heading">Hello {`${fname}  ${lname}`}</h1>
-    <div>
-      <img
-        className="food-img"
-        src="https://static.toiimg.com/thumb/69038542.cms?imgsize=563452&width=800&height=800"
-        alt="food"
-      />
-      <img
-        className="food-img"
-        src="https://static.toiimg.com/thumb/69038542.cms?imgsize=563452&width=800&height=800"
-        alt="food"
-      />
-      <img
-        className="food-img"
-        src="https://static.toiimg.com/thumb/69038542.cms?imgsize=563452&width=800&height=800"
-        alt="food"
-      />
-    </div>
-    <p>And my lucky number is {lucky}</p>
+    <h1 className="heading" style={customeStyle}>
+      {greetings}
+    </h1>
   </div>,
   document.getElementById("root")
 );
