45bd173db6e85a6f8beb5b73db37e85ba7bedef3
Abdulhamid-Osman <47813658+Abdulhamid-Osman@users.noreply.github.com>
2020-09-16 09:30:43 +0530

    JSX styling elements

--- src/index.js
+++ src/index.js
@@ -6,7 +6,24 @@ const lname = "Osman";
 const lucky = Math.floor(Math.random() * 100);
 ReactDom.render(
   <div>
-    <h1>Hello {`${fname}  ${lname}`}</h1>
+    <h1 className="heading">Hello {`${fname}  ${lname}`}</h1>
+    <div>
+      <img
+        className="food-img"
+        src="https://static.toiimg.com/thumb/69038542.cms?imgsize=563452&width=800&height=800"
+        alt="food"
+      />
+      <img
+        className="food-img"
+        src="https://static.toiimg.com/thumb/69038542.cms?imgsize=563452&width=800&height=800"
+        alt="food"
+      />
+      <img
+        className="food-img"
+        src="https://static.toiimg.com/thumb/69038542.cms?imgsize=563452&width=800&height=800"
+        alt="food"
+      />
+    </div>
     <p>And my lucky number is {lucky}</p>
   </div>,
   document.getElementById("root")
